df113f09bb78a43150961f2af6ad92172ce2e7ad
Sergei Savkin <ssavkin@dairymaster.com>
2023-01-29 21:16:33 +0000

    first import lora project

---
+++ Core/Inc/lora_track.h
@@ -0,0 +1,18 @@
+/**
+ * @file lora_track.h
+ * @author Sergei Savkin (ssavkin@dairymaster.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-01-29
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef LORA_TRACK_H
+#define LORA_TRACK_H
+
+int LoraTrackInit(void);
+int LoraTrackMainLoop(void);
+
+#endif /* LORA_TRACK_H */
--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -34,18 +34,6 @@ extern "C" {
 #include "app_entry.h"
 #include "app_common.h"
 
-#include "stm32wbxx_ll_spi.h"
-#include "stm32wbxx_ll_bus.h"
-#include "stm32wbxx_ll_cortex.h"
-#include "stm32wbxx_ll_rcc.h"
-#include "stm32wbxx_ll_system.h"
-#include "stm32wbxx_ll_utils.h"
-#include "stm32wbxx_ll_pwr.h"
-#include "stm32wbxx_ll_gpio.h"
-#include "stm32wbxx_ll_dma.h"
-
-#include "stm32wbxx_ll_exti.h"
-
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
